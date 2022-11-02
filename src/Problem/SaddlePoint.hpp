edb5c362cba112053e9384e0cae1c6f185344fb3
DrEden33773 <edwardwang33773@gmail.com>
2022-11-02 22:05:02 +0800

    update

--- src/Problem/SaddlePoint.hpp
+++ src/Problem/SaddlePoint.hpp
@@ -11,19 +11,40 @@
 
 #pragma once
 
-#include <initializer_list>
 #include <iostream>
+#include <unordered_map>
 #include <vector>
 
 class SaddlePoint {
     std::vector<std::vector<int>> Data;
 
-    int Row = 0;
-    int Col = 0;
+    int Sizeof_Row = 0;
+    int Sizeof_Col = 0;
+
+    std::unordered_map<int, int> min_in_row;
+    std::unordered_map<int, int> max_in_col;
+
+    SaddlePoint() = default;
 
 public:
-    SaddlePoint(std::initializer_list<
-                std::initializer_list<int>>&& init) {
-        // TODO(eden):
+    explicit SaddlePoint(const std::vector<std::vector<int>>& init)
+        : Sizeof_Row(static_cast<int>(init.size()))
+        , Sizeof_Col(static_cast<int>(init.begin()->size())) {
+        std::vector<int> input_sub;
+        Data.reserve(Sizeof_Row);
+        input_sub.reserve(Sizeof_Col);
+        for (auto&& sub : init) {
+            for (auto&& elem : sub) {
+                input_sub.emplace_back(elem);
+            }
+            Data.emplace_back(input_sub);
+            input_sub.clear();
+        }
+    }
+    void process() {
+    }
+
+    static void solution(const std::vector<std::vector<int>>& init) {
+        SaddlePoint TheSolution(init);
     }
 };
