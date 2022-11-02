35b9118b4af9d0ca1d6b716a644fa8e9e41359f1
DrEden33773 <edwardwang33773@gmail.com>
2022-11-02 22:23:00 +0800

    first version of `SaddlePoint`

--- src/Problem/SaddlePoint.hpp
+++ src/Problem/SaddlePoint.hpp
@@ -21,8 +21,18 @@ class SaddlePoint {
     int Sizeof_Row = 0;
     int Sizeof_Col = 0;
 
+    // { row_index : min_number }
+    // row_index => [0, Sizeof_Row)
     std::unordered_map<int, int> min_in_row;
+    // { col_index : max_number }
+    // col_index => [0, Sizeof_Col)
     std::unordered_map<int, int> max_in_col;
+
+    // location_of_SaddlePoint
+    // { row, col }
+    // row => [1, Sizeof_Row]
+    // col => [1, Sizeof_Col]
+    std::vector<std::pair<int, int>> location;
 
     SaddlePoint() = default;
 
@@ -42,9 +52,46 @@ public:
         }
     }
     void process() {
+        // 1. Init two tables
+        // min_in_row => Data[all in 0..Sizeof_Row][0]
+        // max_in_col => Data[0][all in 0..Sizeof_Col]
+        for (int row_idx = 0; row_idx < Sizeof_Row; ++row_idx) {
+            min_in_row[row_idx] = Data[row_idx][0];
+        }
+        for (int col_idx = 0; col_idx < Sizeof_Col; ++col_idx) {
+            max_in_col[col_idx] = Data[0][col_idx];
+        }
+        // 2. Traverse & Update
+        for (int row_idx = 0; row_idx < Sizeof_Row; ++row_idx) {
+            for (int col_idx = 0; col_idx < Sizeof_Col; ++col_idx) {
+                int curr_elem       = Data[row_idx][col_idx];
+                min_in_row[row_idx] = std::min(min_in_row[row_idx], curr_elem);
+                max_in_col[col_idx] = std::max(max_in_col[col_idx], curr_elem);
+            }
+        }
+        // 3. select
+        for (int row_idx = 0; row_idx < Sizeof_Row; ++row_idx) {
+            for (int col_idx = 0; col_idx < Sizeof_Col; ++col_idx) {
+                if (min_in_row[row_idx] == max_in_col[col_idx]) {
+                    int row = row_idx + 1;
+                    int col = col_idx + 1;
+                    location.push_back(std::make_pair(row, col));
+                }
+            }
+        }
+    }
+    void output() {
+        std::cout << "SaddlePoints are => " << std::endl;
+        for (auto&& pair : location) {
+            std::cout << "(" << pair.first << ", " << pair.second << ")";
+            std::cout << std::endl;
+        }
+        std::cout << std::endl;
     }
 
     static void solution(const std::vector<std::vector<int>>& init) {
         SaddlePoint TheSolution(init);
+        TheSolution.process();
+        TheSolution.output();
     }
 };
