11b856ca97d53d32db7543aff5396ed75262ec7c
DrEden33773 <edwardwang33773@gmail.com>
2022-11-07 22:07:04 +0800

    update

--- src/DS/UndirectedGraph.hpp
+++ src/DS/UndirectedGraph.hpp
@@ -11,6 +11,7 @@
 
 #pragma once
 
+#include <stdexcept>
 #include <unordered_map>
 #include <unordered_set>
 #include <utility>
@@ -29,6 +30,7 @@ private:
     std::unordered_map<int, T>    Index_V_Map;
     int                           size = 0;
 
+    UndirectedGraph() = default;
     void copy_from(const UG& copied) {
         Mat         = copied.Mat;
         V_Index_Map = copied.V_Index_Map;
@@ -41,7 +43,6 @@ private:
         Index_V_Map = std::move(moved.Index_V_Map);
         size        = std::move(moved.size);
     }
-
     void init_mat(const int& the_size) {
         Mat = MatType(the_size, MatRowType(the_size, 0));
     }
@@ -71,8 +72,8 @@ public:
         return *this;
     }
 
-public:
     /// @b constructor
+public:
     using ConstructList = std::vector<std::pair<T, T>>;
     explicit UndirectedGraph(const ConstructList& init) {
         std::unordered_set<T> V_Set;
@@ -101,10 +102,60 @@ public:
         for (const std::pair<T, T>& V_pair : init) {
             int first_idx  = V_Index_Map[V_pair.first];
             int second_idx = V_Index_Map[V_pair.second];
-
+            // set => Mat[][] = 1
             Mat[first_idx][second_idx] = 1;
             Mat[second_idx][first_idx] = 1;
         }
+    }
+
+    /// @b vertex_operations
+public:
+    bool if_has_vex(const T& v_name) {
+        if (!V_Index_Map.contains(v_name)) {
+            return false;
+        }
+        return true;
+    }
+    bool if_has_index(const int& index) {
+        if (index < 0 || index >= size) {
+            return false;
+        }
+        return true;
+    }
+
+    int GetIndex(const T& v_name) {
+        int res = -1;
+        if (V_Index_Map.contains(v_name)) {
+            res = V_Index_Map[v_name];
+        }
+        return res;
+    }
+    T GetVex(const int& index) {
+        if (index < 0 || index >= size) {
+            throw std::out_of_range("Input `index` is out of range!");
+        }
+        return Index_V_Map[index];
+    }
+
+    int FindAdjIndex(const int& index) {
+        if (!if_has_index(index)) {
+            return -1;
+        }
+        int index_ans = -1;
+        for (int col_idx = 0; col_idx < size; ++col_idx) {
+            if (Mat[index][col_idx]) {
+                index_ans = col_idx;
+                break;
+            }
+        }
+        return index_ans;
+    }
+    int FindAdjIndex(const T& v_name) {
+        if (!if_has_vex(v_name)) {
+            return -1;
+        }
+        int idx_of_input = GetIndex(v_name);
+        return FindAdjIndex(idx_of_input);
     }
 };
 
